f789ecb1066680a0f6ccf6b1ca24e3ddf7bfcc46
Andrew Ferguson <fergusonandrew4@gmail.com>
2017-08-13 15:59:51 -0400

    working simple version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import moment from 'moment';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: FirebaseListObservable<any[]>;
+  entry: any = {};
+  date: any;
+  queryObservable: FirebaseListObservable<any[]>;
+  sizeSubject: Subject<any>;
+  balance: number;
 
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+    this.items = db.list('/entries');
+    this.sizeSubject = new Subject();
+    this.queryObservable = db.list('/entries', {
+      query: {
+        orderByChild: 'amount'
+      }
+    });
+    this.date = moment().toISOString();
+    this.sum();
+  }
+
+  save(label: string, amount: number, selectedDate: string) {
+    this.items.push({
+      label: label,
+      amount: amount,
+      date: selectedDate
+    });
+    this.sum();
+  }
+  sum() {
+    this.queryObservable.subscribe(queryReturn => {
+      this.balance = queryReturn.reduce((acc, entry) => {
+        return acc + Number(entry.amount);
+      }, 0);
+    });
+  }
+  delete(entryKey: string) {
+    this.items.remove(entryKey);
   }
 
 }
